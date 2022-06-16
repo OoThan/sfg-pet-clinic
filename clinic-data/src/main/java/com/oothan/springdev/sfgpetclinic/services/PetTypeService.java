cb464025d9f04fcf39cdda3d450eaa6406d66f1d
OoThan <mgoothan.psnl@gmail.com>
2022-06-16 18:23:21 +0800

    Added PetType Service close #31

---
+++ clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.oothan.springdev.sfgpetclinic.services;
+
+import com.oothan.springdev.sfgpetclinic.models.PetType;
+
+/**
+ * Created By oothan on Jun, 2022
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
