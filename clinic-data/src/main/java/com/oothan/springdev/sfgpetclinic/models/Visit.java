cd66743d559cb139b5267baaa50c54adc3f14253
OoThan <mgoothan.psnl@gmail.com>
2022-06-16 16:56:45 +0800

    added Visit, updated owner with pet relationship close #28

---
+++ clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/models/Visit.java
@@ -0,0 +1,37 @@
+package com.oothan.springdev.sfgpetclinic.models;
+
+import java.time.LocalDate;
+
+/**
+ * Created By oothan on Jun, 2022
+ */
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
