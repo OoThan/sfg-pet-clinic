9612155b931e5d0d1f65925aa07e7e7493d14338
OoThan <mgoothan.psnl@gmail.com>
2022-06-17 11:56:24 +0800

    Created Speciality Map Service close #33

---
+++ clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package com.oothan.springdev.sfgpetclinic.services;
+
+import com.oothan.springdev.sfgpetclinic.models.Speciality;
+
+/**
+ * Created By oothan on Jun, 2022
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
