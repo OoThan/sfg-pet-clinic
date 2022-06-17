c2a7a0aa61a3b3e2719a0fd344f093be89cfede5
OoThan <mgoothan.psnl@gmail.com>
2022-06-17 12:50:30 +0800

    Added Speciality to Vets on startup close #36

--- clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/services/map/VetServiceMap.java
+++ clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.oothan.springdev.sfgpetclinic.services.map;
 
+import com.oothan.springdev.sfgpetclinic.models.Speciality;
 import com.oothan.springdev.sfgpetclinic.models.Vet;
+import com.oothan.springdev.sfgpetclinic.services.SpecialityService;
 import com.oothan.springdev.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,13 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -20,6 +29,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpecialities().size() > 0) {
+            object.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null) {
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
