8291876bcd887a0a81b16c4745bdbe02b1b5ee96
OoThan <mgoothan.psnl@gmail.com>
2022-06-26 21:19:31 +0630

    Create Visit Entity Close #40

--- clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/models/Visit.java
+++ clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/models/Visit.java
@@ -1,14 +1,23 @@
 package com.oothan.springdev.sfgpetclinic.models;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created By oothan on Jun, 2022
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
