985e63646f6e839d744d7e1cfb8c6394614f4ee1
OoThan <mgoothan.psnl@gmail.com>
2022-06-26 21:09:09 +0630

    Convert Vets to JPA Entities Close #39

--- clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/models/Speciality.java
+++ clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/models/Speciality.java
@@ -1,10 +1,17 @@
 package com.oothan.springdev.sfgpetclinic.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created By oothan on Jun, 2022
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
