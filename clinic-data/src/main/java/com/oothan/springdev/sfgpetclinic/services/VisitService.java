a3f153e064924d14aef28dc8166c38a8eb0f8d21
OoThan <mgoothan.psnl@gmail.com>
2022-07-05 00:04:52 +0630

    Create Visit Map Service Close #48

---
+++ clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package com.oothan.springdev.sfgpetclinic.services;
+
+import com.oothan.springdev.sfgpetclinic.models.Visit;
+
+/**
+ * Created By oothan on Jul, 2022
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
