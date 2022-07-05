0e9ab3a7aa66a5114dd72b30dc22c2de52ee3e1c
OoThan <mgoothan.psnl@gmail.com>
2022-07-05 20:07:14 +0630

    Project Lombok and Refactor Close #53

--- clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/models/Speciality.java
+++ clinic-data/src/main/java/com/oothan/springdev/sfgpetclinic/models/Speciality.java
@@ -1,5 +1,7 @@
 package com.oothan.springdev.sfgpetclinic.models;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -7,18 +9,15 @@ import javax.persistence.Table;
 /**
  * Created By oothan on Jun, 2022
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
     @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
